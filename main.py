7c4b043cffb3f894a9ad3700ef281b26d0ab4d04
ClaudiuA-creator <82838330+ClaudiuA-creator@users.noreply.github.com>
2021-04-20 00:11:32 +0300

    Selectarea autoritatiilor resp. de implementarea masurilor

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-URL = 'https://www.calitateaer.ro/public/home-page/?__locale=ro'
+import re
+
+URL = 'https://www.calitateaer.ro/public/management-page/management-page/?__locale=ro'
+
 page = requests.get(URL)
+
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+
+consilii = re.findall(r'[j]\w{3,}\s\w{3,}', soup.prettify())
+for consilii in consilii:
+    print(consilii)
+
+#print(soup.prettify())
