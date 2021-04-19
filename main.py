74685b984fd0ff7fde38cbbf10d666a1ed25b7e1
ClaudiuA-creator <82838330+ClaudiuA-creator@users.noreply.github.com>
2021-04-20 00:48:19 +0300

    Incercam sa selectam in continuare consiliile judetene si sa le afisam in ordine intr-un fisier .txt

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-consilii = re.findall(r'[j]\w{3,}\s\w{3,}', soup.prettify())
+consilii = re.findall(r'[C]\w{7,12}\s\w{3,}\s\w{3,}', soup.prettify())
 for consilii in consilii:
     print(consilii)
+
+f = open("consil.txt", "w")
+for consilii in consilii:
+    f.write(consilii+"\n")
 
 #print(soup.prettify())
