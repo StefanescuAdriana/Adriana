6a7f7a0ad58a5c2a67fa51ba87d16811d2153891
BoranDani <82838308+BoranDani@users.noreply.github.com>
2021-05-19 15:55:16 +0300

    Incercare grafic

--- Grafic.py
+++ Grafic.py
@@ -3,15 +3,15 @@ import csv
 
 from main import row
 
-Subjects = []
-Scores = []
+Judete = []
+Calitatea_aerului = []
 
 with open('tabel2.csv', 'r') as csvfile:
     lines = csv.reader(csvfile, delimiter=',')
     for lines in row:
-        Subjects.append(lines[0])
-        Scores.append(int(lines[1]))
+        Judete.append(lines[0])
+        Calitatea_aerului.append(int(lines[1]))
 
-plt.pie(Scores, labels=Subjects, autopct='%.2f%%')
+plt.pie(Calitatea_aerului, labels=Judete, autopct='%.2f%%')
 plt.title('calitate aer', fontsize=20)
 plt.show()
