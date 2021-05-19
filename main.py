1cd8e1393ec777c5de2a697e18e871bf350f3321
ClaudiuA-creator <82838330+ClaudiuA-creator@users.noreply.github.com>
2021-05-19 15:35:26 +0300

    Last update stocare date in tabele.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from openpyxl import Workbook
 import csv
+import pandas as pd
 
 #--------------------Preluam HTML-ul-------------------------------------------
 
@@ -12,25 +13,58 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 #--------------------Creeam fisierul pentru stocarea datelor-------------------
 
-filename = 'test.csv'
+filename = 'tabel.csv'
 f = open(filename, "w", encoding="utf-8")
-f.write("datele preluate sunt: "+"\n")
+f.write("Datele preluate sunt: "+"\n")
+
+#---------------------Al doilea fisier ajutator pentru stocare-----------------
+
+filename = 'tabel2.csv'
+g = open(filename, "w", encoding="utf-8")
 
 #--------------------Dam scrape la datele din tabel folosind datele din HTMl------
 
 for heading in soup.find_all('h3', class_='post-title'):
-    print(heading.text)
+    #print(heading.text + "\t")
     f.write(heading.text)
 
-    for tr in soup.find_all('tr'):
-        print(tr.text)
-        f.write(tr.text)
+    for aer_table in soup.find_all('table', class_='table table-striped table-bordered'):
+
+        for table_bod in aer_table.find_all('tbody'):
+            rows = table_bod.find_all('tr')
+
+            for tr in rows:
+                locatii = tr.find('td', class_= 'text-center')
+                #print(tr.text)
+                f.write(tr.text)
+
+#---------------------- Mai luam datele pentru o noua prelucrare----------------------
+
+for heading in soup.find_all('h3', class_='post-title'):
+    print(heading.text + "\t")
+    g.write(heading.text+"\n")
+    print()
+
+for tbod in soup.find_all('tr'):
+    valori = [dt.text for dt in tbod.find_all('p')]
+    print(valori)
+    g.write(str(valori)+"\n")
+
+for tr in soup.find_all('tr'):
+    values = [td.text for td in tr.find_all('td')]
+    print(values)
+    g.write(str(values)+"\n")
 
 #-------------------------------Creeam fisierul XSlX----------------------------------
 
 wb = Workbook()
 ws = wb.active
-with open('test.csv', 'r+', encoding="utf-8") as f:
+with open('tabel.csv', 'r+', encoding="utf-8") as f:
     for row in csv.reader(f):
         ws.append(row)
-wb.save('name.xlsx')
+wb.save('tabel.xlsx')
+
+with open('tabel2.csv', 'r+', encoding="utf-8") as g:
+    for row in csv.reader(g):
+        ws.append(row)
+wb.save('tabel2.xlsx')
